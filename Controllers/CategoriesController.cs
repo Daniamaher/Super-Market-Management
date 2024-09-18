a3586195fe3f96d59bf06294e38664a5acba0cd0
Daniamaher <148376324+Daniamaher@users.noreply.github.com>
2024-09-18 15:18:07 +0300

    Add files via upload

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using supermarketsys.Models;
+
+namespace supermarketsys.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var category = CategoriesRepository.GetCategories();
+            return View(category);
+        }
+
+
+
+        public IActionResult Edit(int? id)
+        {
+            ViewBag.Action = "edit";
+            //var category=new Category { CategoryId=id.HasValue ? id.Value : 0 };
+            var category = CategoriesRepository.GetCategoryById(id.HasValue ? id.Value : 0);
+
+            return View(category);
+
+        }
+
+        
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.UpdateCategory(category.CategoryId, category);
+                return RedirectToAction("Index");
+            }
+
+            return View(category); 
+        }
+
+
+
+
+
+
+
+
+
+
+        public IActionResult Add()
+        {
+            ViewBag.Action = "add";
+
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.AddCategory(category);
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+
+
+             public IActionResult Delete(int categoryId) {
+            CategoriesRepository.DeleteCategory(categoryId);
+            return RedirectToAction("Index");
+
+
+
+        }
+
+
+
+
+    }
+}
